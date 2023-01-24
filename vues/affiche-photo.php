d6bee580108839f1ff32a9b5d6a0529f33c5b3c3
Stephane_tcha <stephanetcha@gmail.com>
2023-01-24 12:50:51 +0100

    organisation de la couche Vue

---
+++ vues/affiche-photo.php
@@ -0,0 +1,15 @@
+<?php
+$titre = 'Une photo de mon book';
+if (is_null($photo)):
+    $contenu = "Cette photo n'existe pas.";
+else:
+    ob_start();
+?>
+        <article>
+            <img src="photos/<?= $photo->fichier ?>" width="500" />
+            <h2><?= $photo->titre ?></h2>
+        </article>
+<?php
+    $contenu = ob_get_clean();
+endif;
+require_once('layout.php');
