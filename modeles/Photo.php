bd6c9c2f4b904be72c2f47d75c95995c482ced9a
Stephane_tcha <stephanetcha@gmail.com>
2023-02-21 19:36:58 +0100

    Photo.php

---
+++ modeles/Photo.php
@@ -0,0 +1,34 @@
+<?php
+class Photo
+{
+    use Modele;
+    private $id;
+    private $fichier;
+    private $titre;
+    public function afficherPhoto($id)
+    {
+        if (!is_null($this->pdo)) {
+            $stmt = $this->pdo->prepare('SELECT * FROM photo WHERE id = ?');
+        }
+        $photo = null;
+        if ($stmt->execute([$id])) {
+            $photo = $stmt->fetchObject('Photo');
+            if(!is_object($photo)) {
+                $photo = null;
+            }
+        }
+        return $photo;
+    }
+    public function getId()
+    {
+        return $this->id;
+    }
+    public function getFichier()
+    {
+        return $this->fichier;
+    }
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+}
